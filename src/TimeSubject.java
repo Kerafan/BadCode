b992b6f2396af7b4632def05ce6ee235a3003971
Kerafan <kerafan@foxmail.com>
2016-12-05 22:22:48 +0800

    Created and wrote class TimeSubject according to the class diagram.

---
+++ src/TimeSubject.java
@@ -0,0 +1,25 @@
+import java.time.Clock;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * FileName: 文件名.java
+ * Function: 功能
+ * History:
+ * <author>      <time>        <version>    <desc>
+ * -------------------------------------------------------------------
+ * Lijinsheng    2016-12-05      1.0          初始版本
+ */
+public abstract class TimeSubject {
+    protected Map<String, Clock> clocks = new HashMap<String, Clock>();
+
+    public void attach(String cityName, Clock clock) {
+        clocks.put(cityName, clock);
+    }
+
+    public void detach(String cityName) {
+        clocks.remove(cityName);
+    }
+
+    public abstract void notifyAllClocks();
+}
