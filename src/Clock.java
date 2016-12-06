8bafc4ebfe0db6304ade7fd1b3218670a19c1f93
Kerafan <kerafan@foxmail.com>
2016-12-06 22:32:19 +0800

    Added constructor Clock(int utcOffset).

--- src/Clock.java
+++ src/Clock.java
@@ -7,9 +7,13 @@
  * Lijinsheng    2016-12-05      1.0          初始版本
  */
 public abstract class Clock {
-    protected static final int UTC_OFFSET = 0;
+    protected static int UTC_OFFSET = 0;
 
     protected int localTime = 0;
+
+    public Clock(int utcOffset) {
+        UTC_OFFSET = utcOffset;
+    }
 
     public abstract void setLocalTime(int localTime);
 
