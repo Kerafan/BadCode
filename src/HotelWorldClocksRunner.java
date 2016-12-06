46e17b1546695773ef534a7a043412270299f925
Kerafan <kerafan@foxmail.com>
2016-12-06 22:16:52 +0800

    Added class HotelWorldClocksRunner with a main() method to have a try.

---
+++ src/HotelWorldClocksRunner.java
@@ -0,0 +1,13 @@
+/**
+ * FileName: 文件名.java
+ * Function: 功能
+ * History:
+ * <author>      <time>        <version>    <desc>
+ * -------------------------------------------------------------------
+ * Lijinsheng    2016-12-06      1.0          初始版本
+ */
+public class HotelWorldClocksRunner {
+    public static void main(String[] args) {
+
+    }
+}
