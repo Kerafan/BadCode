2c93798873aa440d098005e1ceaeaa9e4b3402e6
Kerafan <kerafan@foxmail.com>
2016-12-06 22:41:03 +0800

    Created method Clock.getTime().

--- src/UtcTime.java
+++ src/UtcTime.java
@@ -24,4 +24,10 @@ public class UtcTime extends TimeSubject {
             clock.setLocalTime(Clock.toLocalTime(this.utcZeroTime));
         }
     }
+
+    public void printTimeOfAllClocks() {
+        for (String clockName : super.clocks.keySet()) {
+            System.out.println(clockName + ":" + super.clocks.get(clockName).getTime());
+        }
+    }
 }
