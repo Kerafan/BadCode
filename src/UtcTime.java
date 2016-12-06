3403ae17d83b092b092055c3bef77de1acfa0d67
Kerafan <kerafan@foxmail.com>
2016-12-06 21:51:42 +0800

    Added an int field utcZeroTime to class UtcTime.

--- src/UtcTime.java
+++ src/UtcTime.java
@@ -7,6 +7,8 @@
  * Lijinsheng    2016-12-06      1.0          初始版本
  */
 public class UtcTime extends TimeSubject {
+    private int utcZeroTime;
+
     @Override
     public void notifyAllClocks() {
         for (Clock clock : super.clocks.values()) {
