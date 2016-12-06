79865168538855fa10fd6b3a2ef75a3f34c67737
Kerafan <kerafan@foxmail.com>
2016-12-06 22:12:12 +0800

    Created class CityClock

---
+++ src/CityClock.java
@@ -0,0 +1,10 @@
+/**
+ * FileName: 文件名.java
+ * Function: 功能
+ * History:
+ * <author>      <time>        <version>    <desc>
+ * -------------------------------------------------------------------
+ * Lijinsheng    2016-12-06      1.0          初始版本
+ */
+public class CityClock {
+}
