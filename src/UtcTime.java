5665e206be27a88e6afaf3c4c414e6ca5b6e14ce
Kerafan <kerafan@foxmail.com>
2016-12-06 21:29:47 +0800

    Create class UtcTime

---
+++ src/UtcTime.java
@@ -0,0 +1,14 @@
+/**
+ * FileName: 文件名.java
+ * Function: 功能
+ * History:
+ * <author>      <time>        <version>    <desc>
+ * -------------------------------------------------------------------
+ * Lijinsheng    2016-12-06      1.0          初始版本
+ */
+public class UtcTime extends TimeSubject {
+    @Override
+    public void notifyAllClocks() {
+
+    }
+}
