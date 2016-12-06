c0689d9a04d4d337042c724d5767aeaf0100790f
Kerafan <kerafan@foxmail.com>
2016-12-06 21:56:21 +0800

    Grnerated getter and setter of the field utcZeroTime of class UtcTime

--- src/UtcTime.java
+++ src/UtcTime.java
@@ -9,6 +9,14 @@
 public class UtcTime extends TimeSubject {
     private int utcZeroTime;
 
+    public int getUtcZeroTime() {
+        return utcZeroTime;
+    }
+
+    public void setUtcZeroTime(int utcZeroTime) {
+        this.utcZeroTime = utcZeroTime;
+    }
+
     @Override
     public void notifyAllClocks() {
         for (Clock clock : super.clocks.values()) {
