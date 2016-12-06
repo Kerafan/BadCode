30a74e42bc113e2c3a115ea8852ee9aaf0cbcb8d
Kerafan <kerafan@foxmail.com>
2016-12-06 22:29:05 +0800

    Added constructor CityClock(int utcOffset).

--- src/CityClock.java
+++ src/CityClock.java
@@ -8,6 +8,10 @@
  */
 public class CityClock extends Clock {
 
+    public CityClock(int utcOffset) {
+        super();
+    }
+
     @Override
     public void setLocalTime(int localTime) {
         super.localTime = localTime;
