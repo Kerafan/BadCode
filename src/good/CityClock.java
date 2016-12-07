fb1dc855d2e4fcf53a176f767c84321de3bc6f82
Kerafan <kerafan@foxmail.com>
2016-12-07 23:29:59 +0800

    Made class CityClock use the field utcOffset from the super class Clock.

--- src/good/CityClock.java
+++ src/good/CityClock.java
@@ -1,8 +1,6 @@
 package good;
 
 
-import java.time.Clock;
-
 /**
  * FileName: 文件名.java
  * Function: 功能
@@ -12,16 +10,15 @@ import java.time.Clock;
  * Lijinsheng    2016-12-07      1.0          初始版本
  */
 public class CityClock extends Clock {
-    private int utcOffset;
     private int utcZeroTime;
 
     //TODO-working-on：这里有个传入参数utcOffset，意味着也会有个成员变量叫utOffset，与PhoneClokc的重复了
     public CityClock(int utcOffset) {
-        this.utcOffset = utcOffset;
+        super.utcOffset = utcOffset;
     }
 
     public int getTime() {
-        return (this.utcOffset + this.utcZeroTime + 24) % 24;
+        return (super.utcOffset + this.utcZeroTime + 24) % 24;
     }
 
     public void setUtcZeroTime(int utcZeroTime) {
