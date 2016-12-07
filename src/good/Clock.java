f39ef87a180709479738b4b6838dcf3ec244189b
Kerafan <kerafan@foxmail.com>
2016-12-07 23:28:29 +0800

    Created class Clock.

---
+++ src/good/Clock.java
@@ -0,0 +1,12 @@
+package good;
+
+/**
+ * FileName: 文件名.java
+ * Function: 功能
+ * History:
+ * <author>      <time>        <version>    <desc>
+ * -------------------------------------------------------------------
+ * Lijinsheng    2016-12-07      1.0          初始版本
+ */
+public class Clock {
+}
