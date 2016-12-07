bdcd450737eb39e39849b477bfa04e23efef1100
Kerafan <kerafan@foxmail.com>
2016-12-07 21:39:04 +0800

    Added test the_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() and wrote an assertion.

---
+++ src/good/HotelWorldClockTest.java
@@ -0,0 +1,23 @@
+package good;
+
+import org.junit.Test;
+
+/**
+ * FileName: 文件名.java
+ * Function: 功能
+ * History:
+ * <author>      <time>        <version>    <desc>
+ * -------------------------------------------------------------------
+ * Lijinsheng    2016-12-07      1.0          初始版本
+ */
+public class HotelWorldClockTest {
+    @Test
+    public void the_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        //Arrange
+
+        // Act
+
+        // Assert
+        assertEquals(1, londonClock.getTime());
+    }
+}
