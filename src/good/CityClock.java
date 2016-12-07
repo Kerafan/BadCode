f1a6442aea0fdb12c1658548863d954939edb1f4
Kerafan <kerafan@foxmail.com>
2016-12-07 22:20:17 +0800

    Replacedthe fake value wiht the intention code in method CityClock.getTime().

--- src/good/CityClock.java
+++ src/good/CityClock.java
@@ -9,11 +9,13 @@ package good;
  * Lijinsheng    2016-12-07      1.0          初始版本
  */
 public class CityClock {
+    //TODO：这里有个传入参数utcOffset，意味着也会有个成员变量叫utOffset，与PhoneClokc的重复了
     public CityClock(int utcOffset) {
 
     }
 
+    //TODO-working-on：这里的1与单元测试中的期望值1重复了
     public int getTime() {
-        return 1;
+        return this.utcOffset + this.utcZeroTime;
     }
 }
