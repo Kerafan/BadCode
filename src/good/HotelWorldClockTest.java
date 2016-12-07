161cffe34a41c395ec252fba2a91da418f4d59d7
Kerafan <kerafan@foxmail.com>
2016-12-07 22:38:56 +0800

    Added test:the_time_of_clock_NewWork_should_be_20_after_the_phone_clock_is_set_to_9_Beijing_time and get a negative hour in the actual result.

--- src/good/HotelWorldClockTest.java
+++ src/good/HotelWorldClockTest.java
@@ -1,5 +1,6 @@
 package good;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -14,12 +15,30 @@ public class HotelWorldClockTest {
     @Test
     public void the_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() {
         //Arrange
+        CityClock londonClock = new CityClock(0);
+        PhoneClock phoneClock = new PhoneClock(8);
 
         // Act
         phoneClock.setCityClock(londonClock);
         phoneClock.setTime(9);
 
         // Assert
-        assertEquals(1, londonClock.getTime());
+        Assert.assertEquals(1, londonClock.getTime());
     }
+
+    //TODO-working-on：目前只考虑了比UTC时间早的城市，后续需要考虑比UTC晚的城市
+    @Test
+    public void the_time_of_clock_NewWork_should_be_20_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        // Arrange
+        CityClock newYorkClock = new CityClock(-5);
+        PhoneClock phoneClock = new PhoneClock(8);
+
+        // Act
+        phoneClock.setCityClock(newYorkClock);
+        phoneClock.setTime(9);
+
+        // Assert
+        Assert.assertEquals(20, newYorkClock.getTime());
+    }
+    //TODO：目前只考虑了一个城市，后续要考虑多个城市
 }
