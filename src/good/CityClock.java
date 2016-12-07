34760058d1555a6d26be13b3c255a3929a5124de
Kerafan <kerafan@foxmail.com>
2016-12-07 22:21:57 +0800

    Created field utcOffset in class CityClock and assigned a value to it in the constructor.

--- src/good/CityClock.java
+++ src/good/CityClock.java
@@ -9,9 +9,11 @@ package good;
  * Lijinsheng    2016-12-07      1.0          初始版本
  */
 public class CityClock {
+    private int utcOffset;
+
     //TODO：这里有个传入参数utcOffset，意味着也会有个成员变量叫utOffset，与PhoneClokc的重复了
     public CityClock(int utcOffset) {
-
+        this.utcOffset = utcOffset;
     }
 
     //TODO-working-on：这里的1与单元测试中的期望值1重复了
